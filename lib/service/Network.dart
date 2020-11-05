25448136a5708d501d48d422d3899dc8b0faebd0
Mihail Varbanov <mihail.varbanov@inabyte.com>
2020-11-05 09:14:09 +0200

    Network.RokwireVersion renamed to RokwireAppVersion.

--- lib/service/Network.dart
+++ lib/service/Network.dart
@@ -39,7 +39,7 @@ class Network  {
 
   static const String RokwireApiKey = 'ROKWIRE-API-KEY';
   static const String RokwireHSApiKey = 'ROKWIRE-HS-API-KEY';
-  static const String RokwireVersion = 'V';
+  static const String RokwireAppVersion = 'V';
 
   static final Network _network = new Network._internal();
   factory Network() {
