04f999117054c2e5aae86cd1ffee0bc6e515e0c1
Mihail Varbanov <mihail.varbanov@inabyte.com>
2020-11-26 11:23:33 +0200

    FlexUI switched to Health2 service.

--- lib/service/FlexUI.dart
+++ lib/service/FlexUI.dart
@@ -26,7 +26,7 @@ import 'package:illinois/service/AppLivecycle.dart';
 import 'package:illinois/service/Assets.dart';
 import 'package:illinois/service/Auth.dart';
 import 'package:illinois/service/Config.dart';
-import 'package:illinois/service/Health.dart';
+import 'package:illinois/service/Health2.dart';
 import 'package:illinois/service/Log.dart';
 import 'package:illinois/service/Network.dart';
 import 'package:illinois/service/NotificationService.dart';
@@ -73,8 +73,7 @@ class FlexUI with Service implements NotificationsListener {
       Auth.notifyAuthTokenChanged,
       Auth.notifyCardChanged,
       Auth.notifyUserPiiDataChanged,
-      Health.notifyUserUpdated,
-      Health.notifyUserPrivateKeyUpdated,
+      Health2.notifyUserUpdated,
       AppLivecycle.notifyStateChanged,
     ]);
   }
@@ -100,7 +99,7 @@ class FlexUI with Service implements NotificationsListener {
 
   @override
   Set<Service> get serviceDependsOn {
-    return Set.from([Config(), UserProfile(), Auth(), Health()]);
+    return Set.from([Config(), UserProfile(), Auth(), Health2()]);
   }
 
   @override
@@ -132,8 +131,7 @@ class FlexUI with Service implements NotificationsListener {
     {
       _updateFromNet();
     }
-    else if ((name == Health.notifyUserUpdated) ||
-            (name == Health.notifyUserPrivateKeyUpdated)) {
+    else if ((name == Health2.notifyUserUpdated)) {
       _updateFromNet();
     }
     else if (name == AppLivecycle.notifyStateChanged) {
@@ -418,7 +416,7 @@ class FlexUI with Service implements NotificationsListener {
             result = result && (Auth().isPhoneLoggedIn == value);
           }
           else if ((key == 'healthLoggedIn') && (value is bool)) {
-            result = result && (Health().isUserLoggedIn == value);
+            result = result && (Health2().isUserLoggedIn == value);
           }
           
           else if ((key == 'shibbolethMemberOf') && (value is String)) {
