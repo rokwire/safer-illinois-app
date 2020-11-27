eb22afecacdc8f06346818d091d1133b42190a5b
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2020-11-27 14:06:10 +0200

    Web: read the value of a qr code from image file [#386]

--- web/app.js
+++ web/app.js
@@ -1,23 +1,35 @@
-function alertMessage(text) {
-    alert(text)
-}
-
 function openFileDialog() {
     var input = document.createElement('input');
     input.type = 'file';
-    input.onchange = e => {
-        // getting a hold of the file reference
-        var file = e.target.files[0];
-
-        // setting up the reader
-        var reader = new FileReader();
-        reader.readAsText(file,'UTF-8');
-
-        // here we tell the reader what to do when it's done reading...
-        reader.onload = readerEvent => {
-            //TBD: DD implement reading content
-            console.log('WE LOADED FILE');
-        }
+    input.onchange = event => {
+        onFileSelected(event);
     }
     input.click();
+}
+
+function onFileSelected(event) {
+    // getting a hold of the file reference
+    var file = event.target.files[0];
+
+    // setting up the reader
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    // here we tell the reader what to do when it's done reading...
+    reader.onload = readerEvent => {
+        onFileLoad(readerEvent)
+    }
+}
+
+function onFileLoad(readerEvent) {
+    var content = readerEvent.target.result;
+
+    // set the callback that receives the decoded content as the tasks is async
+    qrcode.callback = function(qrCodeValue) {
+    //TBD: DD - return the value to flutter
+        alert(qrCodeValue);
+    };
+
+    // Start decoding the base64 string
+    qrcode.decode(content);
 }
