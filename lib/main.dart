48821e53253ba195cd42db29792484e80b70dfec
Dobromir Dobrev <dobromir.i.dobrev@gmail.com>
2021-01-08 10:54:32 +0200

    Bring back Health intro pnael to be the first after successful login [#386]

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ import 'package:illinois/service/Localization.dart';
 import 'package:illinois/ui/RootPanel.dart';
 import 'package:illinois/service/Styles.dart';
 import 'package:illinois/ui/onboarding/OnboardingHealthIntroPanel.dart';
-import 'package:illinois/ui/onboarding/OnboardingOrganizationsPanel.dart';
 
 final AppExitListener appExitListener = AppExitListener();
 
@@ -163,7 +162,7 @@ class _AppState extends State<App> implements NotificationsListener {
   Widget get _homePanel {
     if (!Storage().onBoardingPassed) {
       if (Auth().isLoggedIn) {
-        return OnboardingOrganizationsPanel(onboardingContext: {},);
+        return OnboardingHealthIntroPanel(onboardingContext: {},);
       } else {
         return Onboarding().startPanel;
       }
